a44f85c62749aac9a341fcee3b7054ddb63731fa
scoolmb <scoolmabrouki@gmail.com>
2026-08-14 17:58:38 +0100

    devnet analytics enabled

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppProviders } from "@/lib/context/app-providers";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,8 +32,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {" "}
-        <AppProviders>{children}</AppProviders>
+        <Analytics /> <AppProviders>{children}</AppProviders>
       </body>
     </html>
   );
